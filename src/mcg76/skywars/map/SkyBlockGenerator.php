5f5c44c63eb097e7b81cd26673931bd87cbebd61
DavidJBrockway <twitter@pocketserver.co.uk>
2016-02-27 19:37:59 +0000

    Update SkyBlockGenerator.php

--- src/mcg76/skywars/map/SkyBlockGenerator.php
+++ src/mcg76/skywars/map/SkyBlockGenerator.php
@@ -281,11 +281,9 @@ class SkyBlockGenerator extends Generator {
 		}
 		return Block::STONE;
 	}
-	public function populateChunk($chunkX, $chunkZ){
+public function populateChunk($chunkX, $chunkZ){
 		$this->random->setSeed(0xdeadbeef ^ ($chunkX << 8) ^ $chunkZ ^ $this->level->getSeed());
 		foreach($this->populators as $populator){
-			$populator->populate($this->level, $chunkX, $chunkZ, $this->random);
-			$this->random->setSeed(0xdeadbeef ^ ($chunkX << 8) ^ $chunkZ ^ $this->level->getSeed());
 			$populator->populate($this->level, $chunkX, $chunkZ, $this->random);
 		}
 	}
